8c6ec1866ea9dc0a0ea326574f6792ba1d40e02b
jeremysacriz <114373649+jeremysacriz@users.noreply.github.com>
2023-02-05 14:36:30 +1100

    Cart functionality - 5/2/23

--- src/context/reducers.js
+++ src/context/reducers.js
@@ -6,21 +6,64 @@ export const cartReducer = (state, action) => {
 
    switch (action.type) {
       case "ADD_TO_CART":
-         const itemExists = state.cart.find((item) => item.id === action.payload.id && item.size === action.payload.size)
-         if (itemExists) return {
-            ...state,
-            cart: state.cart.filter((product) => 
-               product.id === action.payload.id ? ( product.qty = product.qty + 0.5 ) : product.qty
-            )
+         const itemExists = state.cart.find((item) => item.hasOwnProperty('cartID') && item.size === action.payload.size)
+
+         if (itemExists) {
+            const newCart = state.cart.map((product) => {
+               if (product.cartID === itemExists.cartID) {
+                  return {...product, qty: itemExists.qty + 1}
+               }
+   
+               return product
+            })
+            
+            return {
+               ...state,
+               cart: newCart
+            }
          }
+
+         // Default solution: (Solves +0.5 to qty bug)
+         // if (itemExists) return {
+         //    ...state,
+         //    cart: state.cart.filter((product) => 
+         //       product.id === action.payload.id && product.size === action.payload.size ? ( product.qty = product.qty + 0.5 ) : product.qty
+         //    )
+         // }
+
          return {
             ...state, 
             cart: [...state.cart, {...action.payload, qty: 1, cartID: small_id}],
          }
+
       case "REMOVE_FROM_CART":
+         const cartItem = state.cart.find((item) => item.cartID === action.payload.cartID)
+         if (cartItem.qty > 1) {
+            const updatedCart = state.cart.map((product) => {
+               if (product.cartID === cartItem.cartID) {
+                  return {...product, qty: cartItem.qty - 1}
+               }
+   
+               return product
+            })
+
+            return {
+               ...state,
+               cart: updatedCart
+            }
+         }
+
+         // Default solution: (Solves +0.5 to qty bug)
+         // if (cartItem.qty > 1) return {
+         //    ...state, 
+         //    cart: state.cart.filter((product) => 
+         //       product.cartID === action.payload.cartID ? ( product.qty = product.qty - 0.5 ) : product.qty
+         //    ),
+         // }
+
          return {
             ...state, 
-            cart: state.cart.filter(product => product.id !== action.payload.id),
+            cart: state.cart.filter(product => product.cartID !== action.payload.cartID),
          }
       default:
          return state;
